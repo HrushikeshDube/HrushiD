88b3f33d1f91b59d548bd3f8c6780e1453c7bfab
Hrushi <hrushi8303@gmail.com>
2025-01-01 11:18:53 +0530

    litille chnages

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -27,8 +27,8 @@ const Navbar = () => {
         <ul>
           <li><a href="#about">About</a></li>
           <li><a href="#skills">Skills</a></li>
-          <li><a href="#achievements">Achievements</a></li>
           <li><a href="#projects">Projects</a></li>
+          <li><a href="#achievements">Achievements</a></li>
           <li><a href="#experience">Experience</a></li>
         </ul>
       </div>
